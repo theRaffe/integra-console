1ce034b58cf9b86334fc5b2614c14e3ce65b229c
rafael.briones <rafael.briones.ext@multimedios.net>
2016-12-28 17:01:33 -0600

    *stable version of integral testing including scripts of db

--- integra.console-backend/IntegraConsole/src/main/java/com/izzi/integra/console/web/controller/LogMessageRestController.java
+++ integra.console-backend/IntegraConsole/src/main/java/com/izzi/integra/console/web/controller/LogMessageRestController.java
@@ -8,7 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.text.MessageFormat;
 
@@ -21,13 +24,13 @@ public class LogMessageRestController {
     private static final Logger logger = LoggerFactory.getLogger(LogMessageRestController.class);
 
     @Autowired
-    LogMessageService logMessageService;
+    private LogMessageService logMessageService;
 
 
     @RequestMapping(value = "/getLogMessages", method = RequestMethod.POST)
     public ResponseEntity<?> getLogMessages(@RequestBody final LogMessageRequest logMessageRequest){
         logger.debug(MessageFormat.format("searching log messages {0}", ReflectionToStringBuilder.toString(logMessageRequest)));
-        LogMessageResponse logMessageResponse = logMessageService.getLogMessageByProperties(logMessageRequest);
+        final LogMessageResponse logMessageResponse = logMessageService.getLogMessageByProperties(logMessageRequest);
         return ResponseEntity.ok(logMessageResponse);
     }
 }
